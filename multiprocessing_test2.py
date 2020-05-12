f08ad558d4709208388b4aabaae72f4e9f869744
cdignam8304 <dignachr@gmail.com>
2020-05-12 14:45:37 +0200

    Small updates

--- multiprocessing_test2.py
+++ multiprocessing_test2.py
@@ -17,7 +17,7 @@ def func2(x):
 
 def multiprocessing_func(x, send_end):
     x_squared = x**2
-    time.sleep(0.05)
+    time.sleep(0.1)
     # print(f"{x_squared}")
     send_end.send(x_squared)
 
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     # New example with Pandas
     # =======================
     df = pd.DataFrame(np.arange(start=0, stop=1, step=0.01), columns=list("A"))
-    print(df)
+    # print(df)
     idx_list = list(range(len(df)))
     # print(idx_list)
     
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     #     result = df.loc[i]["A"]**2
     #     # print(result)
     #     result_list.append(result)
-    #     time.sleep(0.05)
+    #     time.sleep(0.1)
     # print(result_list)
     # print(sum(result_list))
     # endtime = time.time()
@@ -51,22 +51,31 @@ if __name__ == "__main__":
     
     # With multiprocessing
     # --------------------
+    
     starttime = time.time()
-    jobs = []
-    pipe_list = []
-    for i in idx_list:
-        recv_end, send_end = multiprocessing.Pipe(False)
-        p = multiprocessing.Process(target=multiprocessing_func, args=(df.loc[i]["A"], send_end))
-        jobs.append(p)
-        pipe_list.append(recv_end)
-        p.start()
+    summary_results = []
+    for run in range(3):
+        
+        print(f"Starting run: {run}")
+        
+        jobs = []
+        pipe_list = []
+        for i in idx_list:
+            recv_end, send_end = multiprocessing.Pipe(False)
+            p = multiprocessing.Process(target=multiprocessing_func, args=(df.loc[i]["A"], send_end))
+            jobs.append(p)
+            pipe_list.append(recv_end)
+            p.start()
+        
+        for proc in jobs:
+            proc.join()
+        result_list = [x.recv() for x in pipe_list]
+        # print(result_list)
+        # summary_results.append(sum(result_list)) # sum of all squared values
+        summary_results.append(result_list[42]) # arbitrarily take 10th value to check order
+        # print(sum(result_list))
     
-    for proc in jobs:
-        proc.join()
-    result_list = [x.recv() for x in pipe_list]
-    print(result_list)
-    print(sum(result_list))
-    
+    print(summary_results)
     endtime = time.time()
     print(f"That took {endtime-starttime} seconds.")
     # That took 0.6258430480957031 seconds.
